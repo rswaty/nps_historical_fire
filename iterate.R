c2e82fb1125718db52d23047fcefce64c8953976
Randy Swaty <39503604+rswaty@users.noreply.github.com>
2025-12-04 09:26:26 -0500

    completed draft dashboard

--- iterate.R
+++ iterate.R
@@ -3,15 +3,15 @@
 
 library(tidyverse)
 
-forests <- unique(data$forestname)
+parks <- unique(data$unit_name)
 
 
 # try to outputs
 purrr::map(
-  .x = forests,  # vector of param values
+  .x = parks,  # vector of param values
   .f = ~rmarkdown::render(
     input = "demo_report.qmd",  # RMarkdown filepath
-    params = list(forestname = .x),  # iterated parameter value
+    params = list(unit_name = .x),  # iterated parameter value
     output_file = stringr::str_glue("output/", .x, "-report.html")  # iterated output path
   )
 )
